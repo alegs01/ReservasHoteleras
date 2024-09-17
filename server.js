f49e6df2d7a73b973ba1ee50a235b7a5188e1052
alegs01 <ale.gomez.saez@gmail.com>
2024-09-17 00:21:41 -0300

    mensaje api-docs

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(express.json());
 // Rutas
 app.use("/api/reservas", require("./routes/reservaRoutes"));
 
+app.get("/", (req, res) => {
+  res.send(
+    "El servidor está en funcionamiento. Puedes consultar la documentación de la API en /api-docs"
+  );
+});
+
 // Configuración de Swagger
 swaggerDocs(app);
 
